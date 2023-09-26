8375b862c7f5a3a4319641c6fff7e1b197d303d6
saaagar10 <143322084+saaagar10@users.noreply.github.com>
2023-09-26 21:53:49 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,108 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int i,j;
+//     for(i=1;i<=5;i++)
+//     {
+//         for(j=1; j<=5;j++)
+//         {
+//             printf("*");
+//         }
+        
+//     printf("\n");}
+    
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int i,j;
+//     for(i=1;i<=5;i++)
+//     {
+//         for(j=1; j<=i;j++)
+//         {
+//             printf("*");
+//         }
+        
+//     printf("\n"); }
+
+    
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int i,j;
+//     for(i=1;i<=5;i++)
+//     {
+//         for(j=1; j<=i;j++)
+//         {
+//             printf("%d" , i);
+//         }
+        
+//     printf("\n");
+//     }
+    
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main()
+// {
+//     int i,j;
+//     for (i=1;i<=5;i++)
+//     {
+//         for(j=1;j<=i;j++)
+//         {
+//             printf("%d" , j);
+//         }
+//         printf("\n");
+//     }
+    
+//     return 0;
+// }
+// {
+//     int i,j,p=0;
+//     for (i=1;i<=5;i++)
+//     {
+//         for(j=1;j<=i;j++)
+//         {
+//             printf("%d" , ++p);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// {
+//     int i,j,p=0;
+//     for (i=1;i<=5;i++)
+//     {
+//         for(j=1;j<=i;j++)
+//         {
+//             printf("%d" ,(6-j));
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+{
+    int i,j,p=0;
+    for (i=5;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double n,s;
+    printf("enter a number ");
+    scanf("%lf" ,&n);
+    s=sqrt(n);
+    printf("the square root of%lf is %lf" ,n,s);
+    return 0;
+    
+}
+
